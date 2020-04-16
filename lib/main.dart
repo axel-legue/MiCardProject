89cced9ca9ba945c399b15cd9564c526ec750ea0
axel.legue <axel.legue31@gmail.com>
2020-04-16 02:01:01 +0200

    feat: start MiCard implementation 	- add profile 	- add custom fonts 	- add name and role

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +9,39 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.orange,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profile.png'),
+              ),
+              Text(
+                'Axel Legué',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter Developper',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.orange.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
-
-
