117c315312aeaccbec048da48cbc3f68f20c4d84
axel.legue <axel.legue31@gmail.com>
2020-04-17 01:59:56 +0200

    feat: add mail and phone number info 	- add Row widget 	- add SizedBox widget 	- add Color Theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.orange,
+        backgroundColor: Colors.blueGrey[500],
         body: SafeArea(
           child: Column(
             children: <Widget>[
@@ -29,13 +29,57 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Text(
-                'Flutter Developper',
+                'FLUTTER DEVELOPPER',
                 style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  fontSize: 20.0,
-                  color: Colors.orange.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.orange.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.fromLTRB(25.0, 40.0, 25.0, 10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.grey[600],
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "06 43 97 20 58",
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.email,
+                    color: Colors.grey[600],
+                    size: 20.0,),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text('axel.legue31@gmail.com',
+                    style: TextStyle(
+                      color:Colors.grey[600],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0
+                    ),)
+                  ],
                 ),
               )
             ],
